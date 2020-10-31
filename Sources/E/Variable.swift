222e9f9a794d398d78a55a306bdecd9f4c24d170
0xLeif <zmeriksen@icloud.com>
2020-10-31 11:53:21 -0500

    Added AnyHashable conformance

--- Sources/E/Variable.swift
+++ Sources/E/Variable.swift
@@ -5,7 +5,7 @@
 //  Created by Zach Eriksen on 9/29/20.
 //
 
-public enum Variable: Equatable, Hashable {
+public enum Variable: Hashable {
     case void
     case bool(Bool)
     case int(Int)
@@ -15,6 +15,52 @@ public enum Variable: Equatable, Hashable {
     case set(Set<Variable>)
     case array([Variable])
     case dictionary([Variable: Variable])
+}
+
+public extension Variable {
+    init() {
+        self = .void
+    }
+
+    init(bool: Bool) {
+        self = .bool(bool)
+    }
+
+    init(int: Int) {
+        self = .int(int)
+    }
+
+    init(float: Float) {
+        self = .float(float)
+    }
+
+    init(double: Double) {
+        self = .double(double)
+    }
+
+    init(string: String) {
+        self = .string(string)
+    }
+
+    init(set: Set<Variable>) {
+        self = .set(set)
+    }
+
+    init(array: [AnyHashable]) {
+        self = .array(array.map({ $0.variable }))
+    }
+
+    init(dictionary: [AnyHashable: AnyHashable]) {
+        let variable = Variable.dictionary([:])
+
+        if case .dictionary(var variable) = variable {
+            dictionary.forEach { (key, value) in
+                variable[value.variable] = value.variable
+            }
+        }
+
+        self = variable
+    }
 }
 
 public extension Variable {
@@ -88,21 +134,51 @@ extension Variable: ExpressibleByStringLiteral {
 }
 
 extension Variable: ExpressibleByArrayLiteral {
-    public init(arrayLiteral: Variable...) {
-        self = .array(arrayLiteral)
+    public init(arrayLiteral: AnyHashable...) {
+        self = .array(arrayLiteral.map({ $0.variable }))
     }
 }
 
 extension Variable: ExpressibleByDictionaryLiteral {
-    public init(dictionaryLiteral elements: (Variable, Variable)...) {
+    public init(dictionaryLiteral elements: (AnyHashable, AnyHashable)...) {
         let dictionary = Variable.dictionary([:])
         
         if case .dictionary(var dictionary) = dictionary {
             elements.forEach { (key, value) in
-                dictionary[key] = value
+                dictionary[value.variable] = value.variable
             }
         }
         
         self = dictionary
+    }
+}
+
+extension AnyHashable {
+    var variable: Variable {
+        if let variable = self as? Variable {
+            return variable
+        } else if let string = self as? String {
+            return .string(string)
+        } else if let int = self as? Int {
+            return .int(int)
+        } else if let float = self as? Float {
+            return .float(float)
+        } else if let double = self as? Double {
+            return .double(double)
+        } else if let bool = self as? Bool {
+            return .bool(bool)
+        } else if let array = self as? [AnyHashable] {
+            return .array(array.map({ $0.variable }))
+        } else if let object = self as? [String: AnyHashable] {
+            var dictionary: [Variable: Variable] = [:]
+            
+            object.forEach { (key: String, value: AnyHashable) in
+                dictionary[.string(key)] = value.variable
+            }
+            
+            return .dictionary(dictionary)
+        } else {
+            return .string(self.description)
+        }
     }
 }
